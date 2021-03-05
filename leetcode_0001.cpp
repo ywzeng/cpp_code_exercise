808a0279c4f7834a93f8b15918e7c53a00eea1e1
ywzeng <ieywzeng@hotmail.com>
2021-03-05 14:19:02 +0800

    两数之和

--- leetcode_0001.cpp
+++ leetcode_0001.cpp
@@ -1,4 +1,4 @@
-class Solution_stupid {
+class Solution_Stupid {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res_vec;
@@ -12,5 +12,26 @@ public:
             }
         }
         return res_vec;
+    }
+};
+
+
+class Solution_Map {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> item_vec;
+        map<int, int> temp_map;
+        for(int i = 0; i < nums.size(); ++i) {
+            temp_map.insert(pair<int, int>(nums[i], i));
+        }
+        for(int i = 0; i < nums.size(); ++i) {
+            auto temp_iter = temp_map.find(target - nums[i]);
+            if(temp_iter != temp_map.end() && temp_iter->second != i) {
+                item_vec.emplace_back(i);
+                item_vec.emplace_back(temp_iter->second);
+                break;
+            }
+        }
+        return item_vec;
     }
 };

89453af268ae7e8681703a804c113604527afaf7
ywzeng <ieywzeng@hotmail.com>
2021-05-07 00:14:14 +0800

    数组异或操作

--- leetcode_1486.cpp
+++ leetcode_1486.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
+        int res = start;
+        for (int i = 1; i < n; ++i) {
+            res ^= start + 2 * i;
+        }
+        return res;
+    }
+};
+
+
+class Solution_stupic {
+public:
+    int xorOperation(int n, int start) {
         vector<int> nums = {start};
         int res = start;
         for (int i = 1; i < n; ++i) {
